addbe2d784094d7a7b8806021141b4e42624ef1e
Siva R <sivagrenganathan+github@protonmail.com>
2018-06-18 00:12:05 +0530

    Add order home page url

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import place_order
+from .views import place_order, home
 
 urlpatterns = [
         path('add', place_order,  name='place_order'),
+        path('', home, name='order_home')
         ]
