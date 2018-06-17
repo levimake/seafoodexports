2d83efdb26f161c327c36a9ae116cf669d68d4fb
Siva R <sivagrenganathan+github@protonmail.com>
2018-06-18 00:12:22 +0530

    Add home page view for orders

--- orders/views.py
+++ orders/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .forms import OrderForm
+from .models import Order
+
+from common.decorators import IsLoggedIn
+
+@IsLoggedIn
+def home(request):
+    if request.method == "GET":
+        my_orders = Order.objects.filter(user = request.session['user_id'])
+        print(my_orders)
+        return render(request, 'myOrders.html', {'orders': my_orders})
 
 
+@IsLoggedIn
 def place_order(request):
     if request.method == "POST":
         form = OrderForm(request.POST)
