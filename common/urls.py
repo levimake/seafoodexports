a9db21a42e12f1989c8f60651ea99a5a25057604
Siva R <sivagrenganathan+github@protonmail.com>
2018-07-10 21:28:57 +0530

    Add endpoints for submit, profile

--- common/urls.py
+++ common/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import register, login, home, logout, error
+from .views import register, login, home, logout, error, profile, profile_view
 
 urlpatterns = [
         path('register', register,  name='register'),
         path('login', login, name='login'),
         path('logout', logout, name='logout'),
+        path('submit', profile, name='profile'),
+        path('profile', profile_view, name='profile_view'),
         path('error', error, name='error'),
-	    path('',home, name='home')
+	path('',home, name='home')
         ]
