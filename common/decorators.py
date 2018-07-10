0a549f13963a79221ee9e4d291ae9197cee5489f
Siva R <sivagrenganathan+github@protonmail.com>
2018-07-10 21:25:45 +0530

    Update decorators

--- common/decorators.py
+++ common/decorators.py
@@ -1,11 +1,12 @@
-from django.shortcuts import redirect
-from .forms import LoginForm
+from django.http import HttpResponseRedirect
 
 def IsLoggedIn(view_func):
     def new_view_func(request):
             if request.session.get('logged_in',False):
+                print("LoggedIn")
                 return view_func(request)
+            
             else:
-                form = LoginForm()
-                return redirect('/login', {'form': form})
+                return HttpResponseRedirect('/login')
+    
     return new_view_func
