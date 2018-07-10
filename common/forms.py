3bfad2997be28df0347db2b2dd7f4ba165e9a3ca
Siva R <sivagrenganathan+github@protonmail.com>
2018-07-10 21:25:58 +0530

    Add new forms for update

--- common/forms.py
+++ common/forms.py
@@ -1,3 +1,5 @@
+from crispy_forms.bootstrap import InlineField
+
 from django import forms
 from .models import User
 
@@ -5,7 +7,7 @@ class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['username', 'email', 'password']
 
 
 class LoginForm(forms.ModelForm):
@@ -13,3 +15,36 @@ class LoginForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+
+class ProfileForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta:
+        model = User
+        widgets = {
+                    'address': forms.Textarea(attrs = {'rows': 3})
+                  }
+        
+        exclude = ['id', 'username', 'email', 'completed']
+
+
+class AddressForm(forms.ModelForm):
+    class Meta:
+        model = User
+        widgets = {
+                    'address': forms.Textarea(attrs = {'rows': 3})
+                  }
+
+        fields = ['address']
+
+
+class PasswordForm(forms.Form):
+    old_password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+
+class PhoneForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['phone_number']
